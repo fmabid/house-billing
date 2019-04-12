70833977daeb57d5b11e59116145ac8cc664b3d8
Md. Faysal Mahmud Abid <fmaabid@gmail.com>
2019-04-12 16:59:01 +0600

    UserFormFragment file

---
+++ app/src/main/java/com/example/madproject/UserFormFragment.java
@@ -0,0 +1,19 @@
+package com.example.madproject;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UserFormFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.user_form_fragment, container, false);
+
+        return view;
+    }
+}
