ff4feca3968ebdba4124fb6f7f930c41c4312d6c
Md. Faysal Mahmud Abid <fmaabid@gmail.com>
2019-04-12 18:12:54 +0600

    done with login fragment

--- app/src/main/java/com/example/madproject/MainActivity.java
+++ app/src/main/java/com/example/madproject/MainActivity.java
@@ -8,34 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements UserFormFragment.FormFragmentListener{
 
-    Button btn_login;
-    EditText username;
-    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_login = findViewById(R.id.btn_login);
-        username = findViewById(R.id.et_username);
-        password = findViewById(R.id.et_password);
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String uname = username.getText().toString();
-                String pass = password.getText().toString();
+    }
 
-                Log.d("Clicked -->   ", uname + " " + pass);
-                if (logedIn(uname, pass)) {
-                    Intent startNextActivity = new Intent(MainActivity.this, HomeActivity.class);
-                    startActivity(startNextActivity);
-                }
-            }
-        });
+    /*  change the sign in/out activity if validation done    */
+    @Override
+    public void changeActivity(String uname, String pass) {
+        if (logedIn(uname, pass)) {
+            Intent startNextActivity = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(startNextActivity);
+        }
     }
 
     /*  Validation  */
