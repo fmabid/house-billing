5b87d9759cd37786042d42d49cbe96bfa27233ca
Md. Faysal Mahmud Abid <fmaabid@gmail.com>
2019-04-26 08:58:29 +0600

    add sign up pages.

--- app/src/main/java/com/example/madproject/SignUpActivity.java
+++ app/src/main/java/com/example/madproject/SignUpActivity.java
@@ -7,48 +7,58 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity{
+public class SignUpActivity extends AppCompatActivity {
 
     Button btn_login;
     Button btn_signup;
     EditText username;
     EditText password;
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
 
         btn_login = findViewById(R.id.btn_login);
         btn_signup = findViewById(R.id.btn_signup);
         username = findViewById(R.id.et_username);
         password = findViewById(R.id.et_password);
+        name = findViewById(R.id.et_woner_name);
 
-        btn_login.setOnClickListener(new View.OnClickListener() {
+        btn_signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String uname = username.getText().toString();
                 String pass = password.getText().toString();
+                String fullName = name.getText().toString();
 
                 // Log.d("Clicked -->   ", uname + " " + pass);
-                if (logedIn(uname, pass)) {
-                    Intent startNextActivity = new Intent(LoginActivity.this, HomeActivity.class);
+                if (sugnUp(uname, pass, fullName)) {
+                    Intent startNextActivity = new Intent(SignUpActivity.this, HomeActivity.class);
                     startActivity(startNextActivity);
                 }
             }
         });
 
-        btn_signup.setOnClickListener(new View.OnClickListener() {
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent startNextActivity = new Intent(LoginActivity.this, SignUpActivity.class);
+                Intent startNextActivity = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(startNextActivity);
             }
         });
     }
 
-    /*  Validation  */
-    public boolean logedIn(String uname, String pass) {
-        return uname.equals("abid") && pass.equals("123");
+
+    /*  Sign up validation  */
+    public boolean sugnUp(String uname, String pass, String fullName) {
+        /*
+        *
+        *   Write here the query for signing up.
+        *
+        * */
+
+        return true;
     }
 }
